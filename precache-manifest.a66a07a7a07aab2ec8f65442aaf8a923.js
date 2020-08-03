ab8ec515b72b5d42a858e544ab384c67fef88554
nimiby <niklas.m.byggmastar@utu.fi>
2020-08-03 18:03:02 +0300

    Updates

---
+++ precache-manifest.a66a07a7a07aab2ec8f65442aaf8a923.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96738f6814bad9055986bce9261371bc",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "301cff18d3f567b26491",
+    "url": "/quiz-app/static/css/main.c1a2ef86.chunk.css"
+  },
+  {
+    "revision": "b07c49ad8ae2b37e7b40",
+    "url": "/quiz-app/static/js/2.8583b97d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/quiz-app/static/js/2.8583b97d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "301cff18d3f567b26491",
+    "url": "/quiz-app/static/js/main.14b2e86a.chunk.js"
+  },
+  {
+    "revision": "e15a1632907c25963bb7",
+    "url": "/quiz-app/static/js/runtime-main.4f483d45.js"
+  }
+]);
